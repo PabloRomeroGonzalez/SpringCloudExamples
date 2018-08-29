0ed4a238bb28cf3a091166f9fccb5f70c081900e
ESX0000427 <pablo.romero@neoris.com>
2018-08-29 15:28:04 +0200

    proyectos de ejemplo

---
+++ lab-6/sentence-server/src/main/java/demo/dao/NounClient.java
@@ -0,0 +1,13 @@
+package demo.dao;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import demo.domain.Word;
+
+@FeignClient("NOUN")
+public interface NounClient {
+
+	@RequestMapping(path= "/", method=RequestMethod.GET)
+	public Word getWord();
+}
